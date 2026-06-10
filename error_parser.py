d2873f87e04185f3ae12bc14f30b856777fd3f4f
Karthik <karthikk1162@gmail.com>
2026-06-10 21:23:05 +0530

    Implemented to extract error line

--- error_parser.py
+++ error_parser.py
@@ -15,3 +15,19 @@ def extract_error_file(traceback_text):
         return matches[-1]
 
     return None
+
+
+def extract_error_line(traceback_text):
+
+    pattern = r'line (\d+)'
+
+    matches = re.findall(
+        pattern,
+        traceback_text
+    )
+
+    if matches:
+
+        return int(matches[-1])
+
+    return None
