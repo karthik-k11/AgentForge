8110c50e69ff9b2d929b805c7363be3c1948ccd0
Karthik <karthikk1162@gmail.com>
2026-08-21 22:46:46 +0530

    Changed the llm model because old one was outdated

--- backend/llm.py
+++ backend/llm.py
@@ -15,7 +15,7 @@ client = Groq(api_key=GROQ_API_KEY)
 def ask_llm(prompt):
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-120b",
         messages=[
             {
                 "role": "user",
